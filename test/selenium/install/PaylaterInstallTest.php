b1f0f20e92437407e93f5cf0243494311426ae5a
cromero <cromero@digitalorigin.com>
2017-07-14 09:37:29 +0200

    selenium install paylater

---
+++ test/selenium/install/PaylaterInstallTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Test\Selenium\Install;
+
+/**
+ * Class WebTest
+ * @package Test\Selenium\Basic
+ */
+class WebTest extends \PHPUnit_Extensions_Selenium2TestCase
+{
+    /**
+     * Configure selenium
+     */
+    protected function setUp()
+    {
+        $this->setBrowser('chrome');
+        $this->setBrowserUrl('http://localhost:4444');
+    }
+
+    /**
+     * testInstallPaylaterInPrestashop17
+     *
+     * @group ps17
+     */
+    public function testInstallPaylaterInPrestashop17()
+    {
+        $username = 'demo@prestashop.com';
+        $password = 'prestashop_demo';
+        $backOfficeLoginUrl = 'http://prestashop17/adminTest/index.php?controller=AdminLogin';
+
+        $this->url($backOfficeLoginUrl);
+
+        $this->byName('email')->value($username);
+        $this->byName('passwd')->value($password);
+        $this->byName('submitLogin')->click();
+
+        sleep(3);
+
+        $content = $this->byTag('h2')->text();
+        $this->assertSame('Dashboard', $content);
+        $this->byId('subtab-AdminParentModulesSf')->click();
+
+        sleep(3);
+
+        $this->byId('page-header-desc-configuration-add_module')->click();
+        $this->byClassName('module-import-start-select-manual')->click();
+        $this->file('Paylater.zip');
+        $this->byId('importDropzone')->submit();
+
+        sleep(10);
+
+        $content = $this->byTag('body')->text();
+        var_dump($content);
+    }
+}
