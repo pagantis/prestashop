8f4056457a335fea139459b725e182af551080dc
cromero <cromero@digitalorigin.com>
2017-08-09 09:07:23 +0200

    updated version to v6.0.7

--- paylater.php
+++ paylater.php
@@ -42,7 +42,7 @@ class Paylater extends PaymentModule
     {
         $this->name = 'paylater';
         $this->tab = 'payments_gateways';
-        $this->version = '6.0.6';
+        $this->version = '6.0.7';
         $this->author = 'Paga+Tarde';
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
