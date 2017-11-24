305a38ec2b6ac0091ea7a0318a0338936228f95e
cromero <cromero@digitalorigin.com>
2017-11-24 07:01:54 +0100

    updated test

--- test/selenium/install/PaylaterPs17InstallTest.php
+++ test/selenium/install/PaylaterPs17InstallTest.php
@@ -87,18 +87,22 @@ class PaylaterPs17InstallTest extends PaylaterPrestashopTest
         $this->findById('PAYLATER_PUBLIC_KEY_PROD')->clear()->sendKeys('pk_this_is_fake');
         $this->findById('PAYLATER_PRIVATE_KEY_PROD')->clear()->sendKeys('this is a fake key');
         $this->findById('frame')->click();
+        $selectorComboSearch = WebDriverBy::name('PAYLATER_IFRAME');
+        /** @var RemoteWebElement[] $selectorElements */
+        $selectorElements = $this->webDriver->findElements($selectorComboSearch);
+        $selectorElements[0]->click();
         $selectorComboSearch = WebDriverBy::name('PAYLATER_PRODUCT_HOOK');
         /** @var RemoteWebElement[] $selectorElements */
         $selectorElements = $this->webDriver->findElements($selectorComboSearch);
-        $selectorElements[4]->click();
+        $selectorElements[3]->click();
         $selectorComboSearch = WebDriverBy::name('PAYLATER_PRODUCT_HOOK_TYPE');
         /** @var RemoteWebElement[] $selectorElements */
         $selectorElements = $this->webDriver->findElements($selectorComboSearch);
-        $selectorElements[2]->click();
+        $selectorElements[1]->click();
         $selectorComboSearch = WebDriverBy::name('PAYLATER_ADD_SIMULATOR');
         /** @var RemoteWebElement[] $selectorElements */
         $selectorElements = $this->webDriver->findElements($selectorComboSearch);
-        $selectorElements[2]->click();
+        $selectorElements[1]->click();
         $this->findById('PAYLATER_PROMOTION_EXTRA')->clear()->sendKeys($this->configuration['extra']);
         $this->findById('module_form_submit_btn');
         $confirmationSearch = WebDriverBy::className('module_confirmation');
