f61cdc4aaef1790ca72803519d2a034df4f1f2f8
Raúl Marín <rmarin@digitalorigin.com>
2018-09-17 16:21:45 +0200

    increase timeout in selenium tests

--- test/PaylaterPrestashopTest.php
+++ test/PaylaterPrestashopTest.php
@@ -55,8 +55,8 @@ abstract class PaylaterPrestashopTest extends TestCase
         $this->webDriver = RemoteWebDriver::create(
             'http://localhost:4444/wd/hub',
             DesiredCapabilities::chrome(),
-            60000,
-            60000
+            120000,
+            120000
         );
     }
 
