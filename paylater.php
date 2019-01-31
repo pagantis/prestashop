b1dac4ad402f4eb34b04067e8c694f3127038596
César Romero <romero.latorre@gmail.com>
2019-01-31 16:27:05 +0100

    Update paylater.php

--- paylater.php
+++ paylater.php
@@ -47,7 +47,7 @@ class Paylater extends PaymentModule
         $this->dotEnvError = null;
         $this->name = 'paylater';
         $this->tab = 'payments_gateways';
-        $this->version = '7.1.0';
+        $this->version = '7.1.1';
         $this->author = 'Paga+Tarde';
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
