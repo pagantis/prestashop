394f3d839decc880ee1e7dd9d60dff223d45d717
rmarinleal <raul.marin.leal@gmail.com>
2019-02-04 23:49:05 +0100

    descriptive unknown exception error

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -229,13 +229,12 @@ class PaylaterPaymentModuleFrontController extends AbstractController
             if ($order instanceof \PagaMasTarde\OrdersApiClient\Model\Order) {
                 $url = $order->getActionUrls()->getForm();
                 $orderId = $order->getId();
-                $result = Db::getInstance()->execute(
-                    "INSERT INTO `" . _DB_PREFIX_ . "pmt_order` (`id`, `order_id`)
+                $sql = "INSERT INTO `" . _DB_PREFIX_ . "pmt_order` (`id`, `order_id`)
                      VALUES ('$cart->id','$orderId') 
-                     ON DUPLICATE KEY UPDATE `order_id` = '$orderId'"
-                );
+                     ON DUPLICATE KEY UPDATE `order_id` = '$orderId'";
+                $result = Db::getInstance()->execute($sql);
                 if (!$result) {
-                    throw new UnknownException('Unable to save pmt-order-id');
+                    throw new UnknownException('Unable to save pmt-order-id in database: '. $sql);
                 }
             } else {
                 throw new OrderNotFoundException();
