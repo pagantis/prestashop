f6c5889010b4c662f8dcf4a2438ef5c953f3fedd
pgarces <37547687+pgarcess@users.noreply.github.com>
2019-08-07 11:56:15 +0200

    change version
    change version

--- pagantis.php
+++ pagantis.php
@@ -68,7 +68,7 @@ class Pagantis extends PaymentModule
     {
         $this->name = 'pagantis';
         $this->tab = 'payments_gateways';
-        $this->version = '8.2.0';
+        $this->version = '8.2.1';
         $this->author = 'Pagantis';
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
