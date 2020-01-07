62386b217ace1b4633ee8dcfc2b6925b9ee17308
rmairnleal <raul.marin.leal@gmail.com>
2020-01-07 11:20:14 +0100

    config files

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
             autoindex: {
                 command:
                     'composer global require pagantis/autoindex \n' +
-                    'php ~/.composer/vendor/pagantis/autoindex/index.php . \n'
+                    'php /home/circleci/.config/composer/vendor/pagantis/autoindex/index.php . \n'
 
             },
             composerProd: {
