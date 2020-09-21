4c159cd22e399b2699dd8b70310a0d37713dab5b
rmarinleal <raul.marin.leal@gmail.com>
2020-09-21 15:51:24 +0200

    update version

--- pagantis.php
+++ pagantis.php
@@ -114,7 +114,7 @@ class Pagantis extends PaymentModule
     {
         $this->name = 'pagantis';
         $this->tab = 'payments_gateways';
-        $this->version = '8.6.7';
+        $this->version = '8.6.8';
         $this->author = 'Pagantis';
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
